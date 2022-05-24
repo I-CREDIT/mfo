ffdbf79a2e93a5255f01dd077edcf09f710ef326
MakhmutT <97080565+MakhmutT@users.noreply.github.com>
2022-05-24 16:57:55 +0600

    IbanUppercase

--- components/registrationForm/ThirdStep.js
+++ components/registrationForm/ThirdStep.js
@@ -60,7 +60,8 @@ class FormRegister extends React.Component {
     this.state = {
       isModalOpen: false,
       checked: false,
-      clicked: false
+      clicked: false,
+      value: ''
     };
     this.toggleModal = this.toggleModal.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -310,6 +311,13 @@ class FormRegister extends React.Component {
   render() {
     const {history} = this.props;
     const ibanMessage = isValidIBANNumber(null || this.props.registration3.iban_account);
+
+    const IbanToUppercase = (e) => {
+      this.setState({
+        value: e.target.value.toUpperCase()
+      })
+    }
+
     return (
       <div>
 
@@ -688,6 +696,8 @@ class FormRegister extends React.Component {
             <div className="input-group">
               <Control
                 component={IbanInput}
+                onInput={(e) => IbanToUppercase(e)}
+                value={this.state.value}
                 model=".iban_account"
                 id="iban_account"
                 placeholder="KZ__________________"
